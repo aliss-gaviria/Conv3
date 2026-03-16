b95ef6b3f554c091272c76d119821a2ea69b42da
Aliss Gaviria <alizzongaviria18@gmail.com>
2026-03-16 04:50:14 +0000

    Conv3

--- src/db.js
+++ src/db.js
@@ -3,12 +3,12 @@ import mongoose from "mongoose";
 
 
 const DB_USER = "admin"; // Usuario de MongoDB
-const DB_PASSWORD = "vagrant"; // Contraseña del usuario
-const DB_HOST = "selene.unicauca.edu.co"; // O IP del servidor MongoDB
-const DB_PORT = "8004"; // Puerto en el que corre MongoDB
+const DB_PASSWORD = "password123"; // Contraseña del usuario
+const DB_HOST = "localhost"; // O IP del servidor MongoDB
+const DB_PORT = "28017"; // Puerto en el que corre MongoDB
 const DB_NAME = "appdb"; // Nombre de la base de datos
 
-const mongoURI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+const mongoURI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=test`;
 export const connectDB = async () => {
     try {
         await mongoose.connect(mongoURI);
